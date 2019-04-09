21d710981ee28257dece01373f712213f1a92e71
eykihlb <eykihlb@163.com>
2019-04-09 12:24:24 +0800

    e

--- src/main/java/com/example/redisdemo/dao/UserMapper.java
+++ src/main/java/com/example/redisdemo/dao/UserMapper.java
@@ -2,8 +2,10 @@ package com.example.redisdemo.dao;
 
 import com.example.redisdemo.entity.User;
 import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Component;
 
 @Mapper
+@Component
 public interface UserMapper {
     int deleteByPrimaryKey(Long id);
 
